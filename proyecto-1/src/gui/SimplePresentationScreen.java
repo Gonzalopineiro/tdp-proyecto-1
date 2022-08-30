e1efef9595f8aa47c338dee27f5d687cbbb29388
Gonzalo Piñeiro <gonzalopineiro.uni@gmail.com>
2022-08-30 12:15:35 -0300

    Cambios en la Gui de proyecto

--- proyecto-1/src/gui/SimplePresentationScreen.java
+++ proyecto-1/src/gui/SimplePresentationScreen.java
@@ -3,12 +3,18 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.time.LocalDateTime;    
+import java.time.format.DateTimeFormatter;  
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Toolkit;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,13 +23,28 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JTextField textField_LU;
+	private JTextField textField_Apellido;
+	private JTextField textField_Nombre;
+	private JTextField textField_Email;
+	private JTextField textField_Github;
+	private JLabel lblLU;
+	private JLabel lblApellido;
+	private JLabel lblNombre;
+	private JLabel lblEmail;
+	private JLabel lblGHURL;
+	private JLabel lblCrecionVentana;
+	private JLabel lblNewLabel;
 
 	public SimplePresentationScreen(Student studentData) {
-		this.studentData = studentData;
 		
+		//Preguntar imagen y gitignore
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png"))); 
+		this.studentData = studentData;
+		ImageIcon pic = new ImageIcon("/proyecto-1/src/images/tdp.png");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(new JLabel(pic));
 		
 		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,11 +56,80 @@ public class SimplePresentationScreen extends JFrame {
 	}
 	
 	private void init() {
+		
+		//Time information
+		DateTimeFormatter formatoDia = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
+		DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");  
+		LocalDateTime tiempoDeCreacion = LocalDateTime.now(); 
+		
+		 
+		contentPane.setLayout(null);
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(5, 5, 446, 212);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
 		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
-		contentPane.add(tabbedPane, BorderLayout.WEST);
+		tabInformation.setLayout(null);
+		
+		textField_LU = new JTextField();
+		textField_LU.setBounds(92, 16, 312, 26);
+		tabInformation.add(textField_LU);
+		textField_LU.setColumns(10);
+		textField_LU.setText(Integer.toString(studentData.getId()));
+		
+		textField_Apellido = new JTextField();
+		textField_Apellido.setBounds(92, 43, 312, 26);
+		tabInformation.add(textField_Apellido);
+		textField_Apellido.setColumns(10);
+		textField_Apellido.setText(studentData.getLastName());
+		
+		textField_Nombre = new JTextField();
+		textField_Nombre.setBounds(92, 69, 312, 26);
+		tabInformation.add(textField_Nombre);
+		textField_Nombre.setColumns(10);
+		textField_Nombre.setText(studentData.getFirstName());
+		
+		textField_Email = new JTextField();
+		textField_Email.setBounds(92, 96, 312, 26);
+		tabInformation.add(textField_Email);
+		textField_Email.setColumns(10);
+		textField_Email.setText(studentData.getMail());
+		
+		textField_Github = new JTextField();
+		textField_Github.setBounds(92, 123, 312, 26);
+		tabInformation.add(textField_Github);
+		textField_Github.setColumns(10);
+		textField_Github.setText(studentData.getGithubURL());
+		
+		lblLU = new JLabel("LU");
+		lblLU.setBounds(19, 21, 61, 16);
+		tabInformation.add(lblLU);
+		
+		lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(19, 48, 61, 16);
+		tabInformation.add(lblApellido);
+		
+		lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(19, 74, 61, 16);
+		tabInformation.add(lblNombre);
+		
+		lblEmail = new JLabel("E-Mail");
+		lblEmail.setBounds(19, 101, 61, 16);
+		tabInformation.add(lblEmail);
+		
+		lblGHURL = new JLabel("GitHub URL");
+		lblGHURL.setBounds(19, 128, 72, 16);
+		tabInformation.add(lblGHURL);
+		contentPane.add(tabbedPane);
+		
+		lblCrecionVentana = new JLabel("Esta ventana fue generada el "+ formatoDia.format(tiempoDeCreacion) + " a las " + formatoHora.format(tiempoDeCreacion) );
+		lblCrecionVentana.setBounds(15, 206, 436, 16);
+		contentPane.add(lblCrecionVentana);
+		
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(SimplePresentationScreen.class.getResource("/images/Foto el 25-8-22 a la(s) 1.32 p. m.-2-2.jpg")));
+		lblNewLabel.setBounds(451, 30, 158, 170);
+		contentPane.add(lblNewLabel);
 	}
 }
--- proyecto-1/src/launcher/Launcher.java
+++ proyecto-1/src/launcher/Launcher.java
@@ -1,12 +1,17 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	Student prueba = new Student(130334, "Pineiro", "Gonzalo", "GonzaloPineiro.uni@mail.com", "https://github.com/MightyPercival", "/proyecto-1/src/images/Foto el 25-8-22 a la(s) 1.32 p. m.-2-2.jpg");
+            	SimplePresentationScreen prueba1 = new SimplePresentationScreen(prueba);
+            	prueba1.setVisible(true);
             }
         });
     }
